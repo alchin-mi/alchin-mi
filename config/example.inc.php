1a8459c53f8c4b623e455fb946e2590106a2cae4
Matvei Alchin <alchin@avtoalfa.com>
2022-09-21 14:56:45 +0300

    Add examlpe to configs file

---
+++ config/example.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+# База данных
+const DBHOST = 'localhost';
+const DBNAME = 'promrukav';
+const DBUSER = 'root';
+const DBPASS = 'root';
+
+# Пагинация
+const PERPAGE = 3;
+const PATTERN = 'page=(:num)';
